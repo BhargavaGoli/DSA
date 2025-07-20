b8aeb108c927eac77085e62953a1af5b0b6d7a13
Bhargava Goli MAC <bhargava7771@gmail.com>
2025-07-20 09:43:36 +0530

    leetcode240

---
+++ python/arrays/binarysearch/leetcode240.py
@@ -0,0 +1,58 @@
+'''
+240. Search a 2D Matrix II
+Solved
+Medium
+
+Topics
+premium lock icon
+Companies
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+Output: true
+Example 2:
+
+
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+Output: false
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= n, m <= 300
+-109 <= matrix[i][j] <= 109
+All the integers in each row are sorted in ascending order.
+All the integers in each column are sorted in ascending order.
+-109 <= target <= 109
+'''
+
+matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
+
+target = 50
+
+rows, columns = len(matrix), len(matrix[0])
+
+l, r = 0, rows - 1
+flag = True
+while l <= r :
+
+    if target == matrix[l][r] :
+        print(True)
+        flag ^= flag
+        break
+    elif target > matrix[l][r] :
+        l += 1
+    else :
+        r -= 1
+    
+if flag :
+    print(False)
